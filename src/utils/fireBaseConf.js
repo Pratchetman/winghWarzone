92f8ac31b8529919f6ad2f5f2a255644bc3a6d22
Manuel Valera <manuelvalera@hotmail.com>
2023-12-19 10:07:13 +0100

    Update fireBaseConf.js

--- src/utils/fireBaseConf.js
+++ src/utils/fireBaseConf.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
     apiKey: "",
-    authDomain: "wingh-eb474.firebaseapp.com",
-    databaseURL: "https://wingh-eb474-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "wingh-eb474",
-    storageBucket: "wingh-eb474.appspot.com",
-    messagingSenderId: "120692076021",
-    appId: "1:120692076021:web:396b11b1a960b65c3d44bc",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
    
   }
 
